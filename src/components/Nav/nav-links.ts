197a3ef936d74b8365de8f5599bcfd13452ee9cf
JottaR <julianojrrodrigues@gmail.com>
2020-12-19 17:31:37 -0300

    Add files via upload

---
+++ src/components/Nav/nav-links.ts
@@ -0,0 +1,52 @@
+export interface NavItem {
+  name: string;
+  path: string;
+  i18nKey: string;
+  saveRequired?: boolean;
+}
+
+const NavItems: NavItem[] = [
+  {
+    name: "Overview",
+    path: "/",
+    i18nKey: "overview-page.title",
+  },
+  {
+    name: "Duplicants",
+    path: "/duplicants",
+    i18nKey: "duplicant.noun_titlecase_plural",
+    saveRequired: true,
+  },
+  {
+    name: "Geysers",
+    path: "/geysers",
+    i18nKey: "geyser.noun_titlecase_plural",
+    saveRequired: true,
+  },
+  // FIXME: Crashes post-dlc
+  // {
+  //   name: "Planets",
+  //   path: "/planets",
+  //   i18nKey: "planet.noun_titlecase_plural",
+  //   saveRequired: true
+  // },
+  {
+    name: "Materials",
+    path: "/materials",
+    i18nKey: "material.noun_titlecase_plural",
+    saveRequired: true,
+  },
+  {
+    name: "Raw Editor",
+    path: "/raw",
+    i18nKey: "raw-editor-page.title",
+    saveRequired: true,
+  },
+  {
+    name: "Changelog",
+    path: "/changelog",
+    i18nKey: "changelog.title",
+  },
+];
+
+export default NavItems;
